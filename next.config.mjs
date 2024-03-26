c902eca686b1e83c0dc1ebcee27dda95a186506d
ShoaibMemon123 <96578536+ShoaibMemon123@users.noreply.github.com>
2024-03-26 16:03:38 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   redirects:async()=>{
     return [
       {
-        source:'/user',
+        source:'/play',
         destination:'https://studentspoint.org/griffith-vice-chancellors-international-scholarship/amp/?utm_source=PillayFB&utm_medium=PKR&utm_campaign=pkr222&amp',
         permanent:true
       }
